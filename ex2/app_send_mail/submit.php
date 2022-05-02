023b7392d154d011b23ff323b24485d6208194c9
Porquezin! <86249854+porquezin@users.noreply.github.com>
2022-05-02 18:42:27 -0300

    Update submit.php

--- ex2/app_send_mail/submit.php
+++ ex2/app_send_mail/submit.php
@@ -49,13 +49,13 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'naomefode2@gmail.com';                     //SMTP username
-    $mail->Password   = 'Contatroll12@';                               //SMTP password
+    $mail->Username   = '???';                     //SMTP username
+    $mail->Password   = '???';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
     //Recipients
-    $mail->setFrom('naomefode2@gmail.com', 'guest');
+    $mail->setFrom('???', 'guest');
     $mail->addAddress($msg->__get('para'));     //Add a recipient
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
